9a3267bdb5547b9f780f13ec2e0e9dad537c6919
gxlpes <guilhermxlopes@gmail.com>
2024-08-07 19:56:17 -0300

    feat: initial project

--- src/components/footer/Footer.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { FooterSection } from "./Styles";
-
-const Footer = () => {
-  return <FooterSection>Project made by Guiherme Lopes</FooterSection>;
-};
-
-export default Footer;
--- src/services/api.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "https://spapi.dev/api",
-});
-
-export default api;
